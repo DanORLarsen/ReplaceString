aa6240d402a97786d0497204ad6f056368fa488f
DanORLarsen <42993978+DanORLarsen@users.noreply.github.com>
2018-09-11 13:09:07 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+         String s = "dog";
+         String replaceF = s.replace('d', 'f');
+
+        System.out.println(replaceF);
+
+         }
+         }
